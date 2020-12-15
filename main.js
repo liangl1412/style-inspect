fa3275229539d719740d71a6b1040a1b5c137ad8
Yi Zhou <Yi.Zhou@epicgames.com>
2020-12-14 16:11:43 -0800

    init

---
+++ main.js
@@ -0,0 +1,32 @@
+function bfs(tree, parentStyle, prop, result) {
+    if (!tree) return;
+    const curStyle = getComputedStyle(tree)[prop] || parentStyle;
+    if (curStyle && curStyle !== parentStyle) {
+        if (result[curStyle]) {
+            result[curStyle] = [...result[curStyle], tree];
+        } else {
+            result[curStyle] = [tree]
+        }
+    }
+    if(tree.childElementCount) {
+        const children = tree.children;
+        for(let i=0; i< children.length; i++ ) {
+            bfs(children[i], curStyle, prop, result)
+        }
+    }
+    
+}
+
+function run () {
+    // start at html tag
+    const styleProps = ['color', 'fontSize', 'lineHeight', 'fontFamily', 'backgroundColor']
+    const startNode = window.document.children[0];
+    console.log('unique style being used in this page');
+    styleProps.forEach((prop) => {
+        const result = {}
+        bfs(startNode, '', prop, result)
+        console.log(prop, result)
+    })
+}
+
+run();
